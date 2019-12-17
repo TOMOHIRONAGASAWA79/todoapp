bfd7479d78b640e6e2b63015cbea95a5dd703b87
TOMOHIRONAGASAWA79 <tomohiro.nagasawa79@gmail.com>
2019-12-17 22:12:24 +0900

    タスク一覧画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-    
-get 'tasks' =>  'task#index'    #tタスクの一覧画面 
+root 'task#index' 
 
+get 'tasks' =>  'task#index'    #タスクの一覧画面 
+post 'tasks' =>  'task#create'  #タスク新規作成機能 
+delete 'tasks/:id'  => 'task#destroy'  #タスク削除機能 
 end
